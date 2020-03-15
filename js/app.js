c3229e377cdbc47de0cbd194ae26460250a289f0
Mauro Longano <mauro.longano@marconirovereto.it>
2020-03-15 23:24:13 +0100

    Visualizza la linea di trend con R^2

--- js/app.js
+++ js/app.js
@@ -1,72 +1,79 @@
+google.charts.load( 'current', { 'packages': [ 'corechart' ] } );
+google.charts.setOnLoadCallback( drawChart );
 const getData = () => {
     //return fetch( "https://coronavirus-tracker-api.herokuapp.com/all" );
     return fetch( "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json" );
 }
-function drawChart ( data, element ) {
-    return () => {
-        let options = {
-            title: "Rapporto dell'incremento del giorno rispetto a quello del giorno prima",
-            hAxis: { title: 'Giorno' },
-            vAxis: { title: 'Ratio' },
-            legend: 'none',
-            trendlines: {
-                0: {
-                    type: 'linear',
-                    showR2: true,
-                    visibleInLegend: true
-                }
+function draw(data, element, options) {
+    var chart = new google.visualization.ScatterChart( document.getElementById( element ) );
+    chart.draw( data, options );
+}
+
+function drawChart () {
+    getData()
+        .then( res => res.json() )
+        .then( json => {
+            let previous = json[ 0 ].nuovi_attualmente_positivi;
+            let data = [
+                [ 'Giorno', 'Ratio' ]
+            ];
+            let totale_casi = [
+                [ 'Giorno', 'Totale' ]
+            ];
+            let nuovi_attualmente_positivi = [
+                [ 'Giorno', 'nuovi' ]
+            ];
+            let terapia_intensiva = [
+                [ 'Giorno', 'intensiva' ]
+            ];
+            let deceduti = [
+                [ 'Giorno', 'Deceduti' ]
+            ];
+            let first = new Date( json[ 0 ].data ).getTime() / 86400000 - 18316;
+            let last = first;
+            for ( daily of json ) {
+                let date = daily.data.split( " " )[ 0 ];
+                console.log( date );
+                let ratio = daily.nuovi_attualmente_positivi / previous;
+                previous = daily.nuovi_attualmente_positivi;
+                console.log( daily );
+                last = new Date( date ).getTime() / 86400000 - 18316;
+                data.push( [ last, ratio ] );
+                totale_casi.push( [ last, daily.totale_casi ] );
+                nuovi_attualmente_positivi.push( [ last, daily.nuovi_attualmente_positivi ] );
+                terapia_intensiva.push( [ last, daily.terapia_intensiva ] );
+                deceduti.push( [ last, daily.deceduti ] );
             }
-        };
 
-        var chart = new google.visualization.ScatterChart( document.getElementById( element ) );
-        chart.draw( google.visualization.arrayToDataTable( data ), options );
-        console.log(options);
-    }
+            data = google.visualization.arrayToDataTable( data );
+            totale_casi = google.visualization.arrayToDataTable( totale_casi );
+            nuovi_attualmente_positivi = google.visualization.arrayToDataTable( nuovi_attualmente_positivi );
+            terapia_intensiva = google.visualization.arrayToDataTable( terapia_intensiva );
+            deceduti = google.visualization.arrayToDataTable( deceduti );
+
+
+            var options = {
+                hAxis: { minValue: first, maxValue:  last},
+                vAxis: { minValue: 0, maxValue: 4 },
+                legend: { left: { position: "bottom-left" } },
+                chartArea: { width: '50%' },
+                trendlines: {
+                    0: {
+                        type: 'linear',
+                        showR2: true,
+                        visibleInLegend: true
+                    }
+                }
+            };
+            draw( data, 'ratio', options );
+
+            options.trendlines[ 0 ].type = 'exponential';
+            options.colors = [ '#6F9654' ];
+            draw( totale_casi, 'totale_casi', options );
+            draw( nuovi_attualmente_positivi, 'nuovi_attualmente_positivi', options );
+            draw( terapia_intensiva, 'terapia_intensiva', options );
+            options.colors = [ '#c21212' ];
+            draw( deceduti, 'deceduti', options );
+
+        } );
 }
-
-getData()
-    .then( res => res.json() )
-    .then( json => {
-        google.charts.load( "current", { packages: [ "corechart" ] } );
-
-        let previous = json[ 0 ].nuovi_attualmente_positivi;
-        var options = {
-            dataAxis: { showMinorLabels: true },
-            legend: { left: { position: "bottom-left" } },
-            start: "2014-06-09",
-            end: "2014-07-03"
-        };
-
-        let items = [];
-
-        let data =  [
-            [ 'Giorno', 'Ratio' ]
-        ];
-
-        for ( daily of json ) {
-            let date = daily.data.split( " " )[ 0 ];
-            console.log( date );
-            let ratio = daily.nuovi_attualmente_positivi / previous;
-            previous = daily.nuovi_attualmente_positivi;
-            console.log( daily );
-            items.push( { x: date, y: ratio } )
-            data.push( [ date, 1.2 ] );
-        }
-/*         var options = {
-            dataAxis: { showMinorLabels: true },
-            legend: { left: { position: "bottom-left" } },
-            start: items[ 0 ].x,
-            end: items[ items.length - 1 ].x
-        };
- */
-        //var dataset = new vis.DataSet( items );
-        //var graph2d = new vis.Graph2d( container, dataset );
-        google.charts.setOnLoadCallback( drawChart( data, 'chart_pointSize' ) );
-
-    } );
-
-const showLatestButtons = ( data ) => {
-    const app = document.querySelector( "#app" );
-
-}
-
