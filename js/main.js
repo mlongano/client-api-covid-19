2246f7b18c000bd4bce3b87eff456d7ff3a6c242
Mauro Longano <mauro@oruam.org>
2020-04-13 04:42:11 +0200

    Riscritta la ricerca nei comuni

--- js/main.js
+++ js/main.js
@@ -3,24 +3,38 @@ Vue.component( "v-select", VueSelect.VueSelect );
 var app = new Vue( {
     el: '#vue',
     data: {
+        dataUrl: `${window.location.origin}/cov19-trentino.json`,
+        json: null,
         selected: null,
         options: [],
         picked: 'm'
     },
     methods: {
-        estraiOpzioni ( json, search, vm = this ) {
+        estraiOpzioni ( json, search, vm ) {
             let re = new RegExp( search, "i" );
 
             // prende i dati dell'ultimo giorno, toglie l'intestazione,
             // filtra sul comune in base alla stringa immessa
             // e ritorna la lista delle opzioni {id: codice, comune: nome_comune}
-            let match = json[ json.length - 1 ].cov19_data.splice( 1 )
+            // gli ultimi dati forniti hanno sono un array con questa struttura:
+            // [
+            //     "codice",
+            //     "nome",
+            //     "contagi",
+            //     "guariti",
+            //     "decessi",
+            //     "aggiornamento",
+            //     "lat",
+            //     "lon"
+            // ]
+            let last = [...json[ json.length - 1 ].cov19_data];
+            let match = last.splice( 1 )
                 .filter( ( row ) => {
                     return row[ 1 ].search( re ) > -1;
                 } )
-                .map( ( row ) => ( { id: row[ 0 ], comune: row[ 1 ] } ) );
+                .map( ( row ) => ( { id: row[ 0 ], comune: row[ 1 ], text: `${row[ 1 ]} (c:${row[ 2 ]}, m:${row[ 4 ]}, g:${row[ 3 ]})`, c: row[ 2 ], m: row[ 4 ], g: row[ 3 ] } ) );
             console.log( "VUE", match );
-            vm.options = match;
+            return match.sort( ( a, b ) => ( parseInt( b[ vm.picked ] ) - parseInt( a[ vm.picked ] ) ) );
         },
 
         onSearch ( search, loading ) {
@@ -28,44 +42,8 @@ var app = new Vue( {
             this.search( loading, search, this );
         },
         search: _.debounce( ( loading, search, vm ) => {
-            /*             if ( !search ) {
-                            vm.options = [];
-                            loading( false );
-                            return;
-                        }
-             */
-            fetch(
-                `${window.location.origin}/cov19-trentino.json`
-            ).then( res => {
-                res.json().then( json => {
-                    let re = new RegExp( search, "i" );
-
-                    // prende i dati dell'ultimo giorno, toglie l'intestazione,
-                    // filtra sul comune in base alla stringa immessa
-                    // e ritorna la lista delle opzioni {id: codice, comune: nome_comune}
-                    // gli ultimi dati forniti hanno sono un array con questa struttura:
-                    // [
-                    //     "codice",
-                    //     "nome",
-                    //     "contagi",
-                    //     "guariti",
-                    //     "decessi",
-                    //     "aggiornamento",
-                    //     "lat",
-                    //     "lon"
-                    // ]
-
-                    let match = json[ json.length - 1 ].cov19_data.splice( 1 )
-                        .filter( ( row ) => {
-                            return row[ 1 ].search( re ) > -1;
-                        } )
-                        .map( ( row ) => ( { id: row[ 0 ], comune: row[ 1 ], text: `${row[ 1 ]} (c:${row[ 2 ]}, m:${row[ 4 ]}, g:${row[ 3 ]})`, c: row[ 2 ], m: row[ 4 ], g: row[ 3 ] } ) );
-                    console.log( "VUE", match );
-                    match.sort( ( a, b ) => ( parseInt( b[ vm.picked ] ) - parseInt( a[ vm.picked]) ) );
-                    vm.options = match;
-                } );
-                loading && loading( false );
-            } );
+            vm.options = vm.estraiOpzioni( vm.json, search, vm );
+            loading && loading( false );
         }, 350 ),
 
         drawComuni () {
@@ -75,12 +53,21 @@ var app = new Vue( {
 
     watch: {
         selected: { handler: 'drawComuni' },
-        picked () {this.onSearch( "" )
-    },
+        picked () {
+            this.onSearch( "" )
+        },
     },
 
     created () {
-        this.onSearch( "" );
+        fetch(
+            this.dataUrl
+        )
+            .then( res => res.json())
+            .then( json => {
+                this.json = json;
+                this.selected = this.estraiOpzioni( json, "ROVERETO", this )[ 0 ];
+                this.options = this.estraiOpzioni( json, "", this );
+            });
         this.drawComuni();
     },
 
