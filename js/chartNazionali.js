f1f9372ed23d79bc0f759f6f3e05b66e9cf694ef
Mauro Longano <mauro@oruam.org>
2020-05-02 20:18:54 +0200

    Aggiorna dati dei Comuni

--- js/chartNazionali.js
+++ js/chartNazionali.js
@@ -16,12 +16,27 @@ function loadNazionaliFactory ( url ) {
     }
 }
 
+Vue.component( "gchart", VueGoogleCharts );
 var chartsNazionali = new Vue( {
     el: '#chartsNazionali',
     data: {
         dataUrl: urlDataItaly,
         lastDate: null,
         json: null,
+        chartData: [
+            [ 'Year', 'Sales', 'Expenses', 'Profit' ],
+            [ '2014', 1000, 400, 200 ],
+            [ '2015', 1170, 460, 250 ],
+            [ '2016', 660, 1120, 300 ],
+            [ '2017', 1030, 540, 350 ]
+        ],
+        chartOptions: {
+            chart: {
+                title: 'Company Performance',
+                subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+            }
+        },
+
         charts: [
             {
                 chart: new Chart( {
